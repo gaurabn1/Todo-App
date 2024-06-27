84b57032bdebdfe9ee10c55938df5e31dc6f34c3
gaurabn1 <gaurabneupane8969@gmail.com>
2024-06-28 00:25:42 +0545

    For Production

--- todo/settings.py
+++ todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-##8io(_!uui7f%(+qrqxdw652rful$%9kb+_c@r8c&3m479tl2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
